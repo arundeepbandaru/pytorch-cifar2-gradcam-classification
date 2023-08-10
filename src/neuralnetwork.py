185201d9de25583503831bb805d9b75d93bf4dee
Arundeep Bandaru <136106571+arundeepbandaru@users.noreply.github.com>
2023-08-10 08:56:29 -0500

    Update neuralnetwork.py

--- src/neuralnetwork.py
+++ src/neuralnetwork.py
@@ -1,28 +1,33 @@
 import torch.nn as nn
 
 
-class CIFARClassifier(nn.Module):
+# Define the neural network model
+class NeuralNetwork(nn.Module):
     def __init__(self):
-        super(CIFARClassifier, self).__init__()  # Invoke parent class initializer
-        self.conv1 = nn.Conv2d(
-            in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1
-        )
-        self.relu = nn.ReLU()  # ReLU activation
-        self.pool = nn.MaxPool2d(
-            kernel_size=2
-        )  # downsizes into 2x2 while preserving important features
-        self.flatten = nn.Flatten()  # flattens the 2D input tensor into a 1D tensor
-        self.fc1 = nn.Linear(
-            16 * 16 * 16, 256
-        )  # first layer: 16*16*16 inputs, 64 outputs
-        self.fc2 = nn.Linear(256, 2)  # output layer: 64 inputs, 2 outputs
+        super(NeuralNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 2)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu(x)
         x = self.pool(x)
-        x = self.flatten(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)  # Flatten
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
         return x
